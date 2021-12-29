33bcfd68aa57fe363d9622c3ee5cfd873738955d
jgomem04 <jgomem04@estudiantes.unileon.es>
2021-12-29 23:38:41 +0100

    About file removed, dashboard file added

--- Front/front_live/src/router/index.js
+++ Front/front_live/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
 import Register from '../views/Register.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
@@ -22,14 +23,10 @@ const routes = [
     name: 'Register',
     component: Register
   },
-
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   }
 ]
 
